36598cb4d8b09f0c5c881f37f5a132e12bd22db4
Katie Stamm <wishingstar0017@yahoo.com>
2015-08-18 17:34:38 -0600

    edit in place task cost

--- public/js/main.js
+++ public/js/main.js
@@ -151,7 +151,7 @@ angular
 		};
 
 		$scope.progress = function(value) {
-			var valueInt = parseInt($scope.value)
+			var valueInt = $scope.value
 			num = 0;
 			var count = num + valueInt
 
@@ -231,7 +231,6 @@ angular
 
 		}, 500);
 
-
 	});
 
 
@@ -290,47 +289,54 @@ angular
 		};
 
 		$scope.count = 0;
+
+
 		
 	});
 
-	// angular
-	// 	.module( 'orderlyApp' )
-	// 	app.directive( 'editInPlace', function() {
-	// 	  return {
-	// 	    restrict: 'E',
-	// 	    scope: { value: '=' },
-	// 	    template: '<span ng-click="edit()" ng-bind="value"></span><input ng-model="value"></input>',
-	// 	    link: function ( $scope, element, attrs ) {
-	// 	      // Let's get a reference to the input element, as we'll want to reference it.
-	// 	      var inputElement = angular.element( element.children()[1] );
+	angular
+		.module( 'orderlyApp' )
+		.directive( 'editInPlace', function() {
+		  return {
+		    restrict: 'E',
+		    scope: { value: '=' },
+		    template: '<span ng-click="edit()" ng-bind="value"></span><input ng-model="value"></input>',
+		    link: function ( $scope, element, attrs ) {
+		      // Let's get a reference to the input element, as we'll want to reference it.
+		      var inputElement = angular.element( element.children()[1] );
 		      
-	// 	      // This directive should have a set class so we can style it.
-	// 	      element.addClass( 'edit-in-place' );
+		      // This directive should have a set class so we can style it.
+		      element.addClass( 'edit-in-place' );
 		      
-	// 	      // Initially, we're not editing.
-	// 	      $scope.editing = false;
+		      // Initially, we're not editing.
+		      $scope.editing = false;
 		      
-	// 	      // ng-click handler to activate edit-in-place
-	// 	      $scope.edit = function () {
-	// 	        $scope.editing = true;
+		      // ng-click handler to activate edit-in-place
+		      $scope.edit = function () {
+		        $scope.editing = true;
 		        
-	// 	        // We control display through a class on the directive itself. See the CSS.
-	// 	        element.addClass( 'active' );
+		        // We control display through a class on the directive itself. See the CSS.
+		        element.addClass( 'active' );
 		        
-	// 	        // And we must focus the element. 
-	// 	        // `angular.element()` provides a chainable array, like jQuery so to access a native DOM function, 
-	// 	        // we have to reference the first element in the array.
-	// 	        inputElement[0].focus();
-	// 	      };
+		        // And we must focus the element. 
+		        // `angular.element()` provides a chainable array, like jQuery so to access a native DOM function, 
+		        // we have to reference the first element in the array.
+		        inputElement[0].focus();
+		      };
 		      
-	// 	      // When we leave the input, we're done editing.
-	// 	      inputElement.prop( 'onblur', function() {
-	// 	        $scope.editing = false;
-	// 	        element.removeClass( 'active' );
-	// 	      });
-	// 	    }
-	// 	  };
-	// 	});
+		      // $scope.save = function(project) {
+
+		      // }
+		      // When we leave the input, we're done editing.
+		      inputElement.prop( 'onblur', function() {
+		        $scope.editing = false;
+		        element.removeClass( 'active' );
+		        
+		      });
+			    // project.$save()
+		    }
+		  };
+		});
 
 angular
 	.module( 'orderlyApp' )
